33693b82e13d712eca28a99b86f54782af41e62f
reza mohammadzadeh <mpcreza@gmail.com>
2023-03-04 10:40:57 +0330

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,26 +18,26 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   bool _autoRotate = false;
 
+  StreamSubscription? autoRotateStreamSubscription;
+
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    init();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
+  Future<void> init() async {
     try {
       _autoRotate = await DeviceAutoRotateChecker.checkAutoRotate();
+      autoRotateStreamSubscription =
+          DeviceAutoRotateChecker.autoRotateStream.listen((bool autoRotate) {
+        _autoRotate = autoRotate;
+        setState(() {});
+      });
     } on PlatformException {
       _autoRotate = false;
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {});
@@ -48,12 +48,18 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Device auto rotate checker'),
         ),
         body: Center(
-          child: Text('Auto rotate: $_autoRotate'),
+          child: Text('Auto rotate is ${_autoRotate ? 'on' : 'off'}'),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    autoRotateStreamSubscription?.cancel();
+    super.dispose();
   }
 }
